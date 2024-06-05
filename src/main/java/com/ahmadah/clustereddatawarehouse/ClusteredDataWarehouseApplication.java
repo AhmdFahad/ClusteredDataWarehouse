ce231ef0aae4b06826634b608b0e1aac2a230373
AhmdFahad <ahmd66699@gmail.com>
2024-06-05 17:15:23 +0300

    Initial commit

---
+++ src/main/java/com/ahmadah/clustereddatawarehouse/ClusteredDataWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.ahmadah.clustereddatawarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusteredDataWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClusteredDataWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ahmadah/clustereddatawarehouse/ClusteredDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmadah.clustereddatawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
