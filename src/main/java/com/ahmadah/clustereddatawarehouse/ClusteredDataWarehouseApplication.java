3f375ef824d2a6fed12d782aa6b9ec4f9737f7ea
AhmdFahad <ahmd66699@gmail.com>
2024-06-05 23:37:26 +0300

    prepare the application| connect to database

--- src/main/java/com/ahmadah/clustereddatawarehouse/ClusteredDataWarehouseApplication.java
+++ src/main/java/com/ahmadah/clustereddatawarehouse/ClusteredDataWarehouseApplication.java
@@ -2,10 +2,16 @@ package com.ahmadah.clustereddatawarehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ClusteredDataWarehouseApplication {
-
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
     public static void main(String[] args) {
         SpringApplication.run(ClusteredDataWarehouseApplication.class, args);
     }
